e833d8616626376fcde8e5bb6ce1f9399fcf5f5d
byngmeister <>
2006-07-28 10:49:14 +0000

    Powerfile ripper script

---
+++ bin/ripper_xml.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+use strict;
+use CMMS::Ripper;
+
+my $ripper = new CMMS::Ripper(conf => '/etc/cmms.conf');
+
+# Lock CD
+#`cdctl -o1`;
+
+my $album = $ripper->metadata;
+
+$ripper->check($album) or &error; # Must unlock draw before dying!
+$ripper->rip($album);
+
+# Unlock CD
+#`cdctl -o0`;
+
+$ripper->encode($album);
+$ripper->cover($album);
+$ripper->store_xml($album);
+$ripper->purge;
+
+# Try to eject CD
+`eject`;
+
+sub error {
+	#`cdctl -o0`;
+	die('Album already ripped');
+}
