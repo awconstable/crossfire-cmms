897f2dd674a84cf48c41b9bb2fb3f180cf3dcb4c
byngmeister <>
2006-08-24 09:01:42 +0000

    RPM bug fix

--- webmin.sh
+++ webmin.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-WEBMIN=`grep -c '/etc/webmin/miniserv.users' setup`
+USER=`grep -c 'setup:' /etc/webmin/miniserv.users`
 
-if [ "$WEBMIN" = "0" ]; then
+if [ "$USER" = "0" ]; then
   echo "Adding setup user to /etc/webmin/miniserv.users"
   echo "setup:$1$56341515$fsIm31epC/wyNZXm7fY4W.:0::" >> /etc/webmin/miniserv.users
+fi
+
+ACCESS=`grep -c 'setup:' /etc/webmin/webmin.acl`
+
+if [ "$ACCESS" = "0" ]; then
+  echo "Adding setup user to /etc/webmin/webmin.acl"
+  echo "setup: cmms net" >> /etc/webmin/webmin.acl
 fi
