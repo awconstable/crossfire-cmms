19c5c087368b91bdf58e5822932339e66c9599f8
byngmeister <>
2006-08-24 15:21:57 +0000

    Empty changelog

--- webmin.sh
+++ webmin.sh
@@ -11,7 +11,22 @@ ACCESS=`grep -c 'setup:' /etc/webmin/webmin.acl`
 
 if [ "$ACCESS" = "0" ]; then
   echo "Adding setup user to /etc/webmin/webmin.acl"
-  echo "setup: cmms net" >> /etc/webmin/webmin.acl
+  echo "setup: net" >> /etc/webmin/webmin.acl
+fi
+
+USR=`grep -c 'cms:' /etc/webmin/miniserv.users`
+
+if [ "$USR" = "0" ]; then
+  echo "Adding CMS user to /etc/webmin/miniserv.users"
+  echo "cms:$1$56432766$WUZFr8kibjGwDryq4N7YJ0:0::" >> /etc/webmin/miniserv.users
+fi
+
+ACS=`grep -c 'cms:' /etc/webmin/webmin.acl`
+
+if [ "$ACS" = "0" ]; then
+  echo "Adding CMS user to /etc/webmin/webmin.acl"
+  echo "cms: cmms" >> /etc/webmin/webmin.acl
 fi
 
 /usr/bin/perl /usr/libexec/webmin/changepass.pl /etc/webmin setup setup
+/usr/bin/perl /usr/libexec/webmin/changepass.pl /etc/webmin cms cms
