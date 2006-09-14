e1fa5cf2f1f44511efc838ab57ad378fd6be92e2
byngmeister <>
2006-09-14 10:30:56 +0000

    Raid check command

---
+++ nagios.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAGIOS=`grep -c 'check_3ware' /etc/nagios/nrpe.cfg`
+
+if [ "$NAGIOS" = "0" ]; then
+  echo "Adding check_3ware command to /etc/nagios/nrpe.cfg"
+  echo "command[check_3ware]=/usr/lib/nagios/plugins/check_3ware" >> /etc/nagios/nrpe.cfg
+fi
