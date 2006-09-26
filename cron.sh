6d4308ca5719f5e2db7538aa14cf58d4b761fe3e
byngmeister <>
2006-09-26 11:04:01 +0000

    Crontab polling entry

--- cron.sh
+++ cron.sh
@@ -5,6 +5,6 @@ CRON=`grep -c '/usr/bin/cmms_poll.pl' /etc/crontab`
 if [ "$CRON" = "0" ]; then
   echo "Adding polling entry to /etc/crontab"
   echo "# CMS ssh connection" >> /etc/crontab
-  echo "10 * * * * root /usr/bin/cmms_poll.pl 1>/dev/null 2>&1" >> /etc/crontab
+  echo "10 * * * * root /usr/bin/perl -w /usr/bin/cmms_poll.pl 1>/dev/null 2>&1" >> /etc/crontab
   /sbin/service crond restart
 fi
